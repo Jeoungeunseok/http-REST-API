f7220cd0573f59a43fbe0124173ed259321647ed
Jeoungeunseok <wjddmstjr65@gmail.com>
2023-08-29 16:48:31 +0900

    rest-api post,put,delete example add

--- lib/utils/http_request.dart
+++ lib/utils/http_request.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -19,8 +20,56 @@ Future<Map<String, dynamic>> httpGet({required String path}) async {
     }
   } catch (e) {
     // 서버가 응답하지 않는 경우
-    // httpFailureNotice();\
-    print("httpGet error: $e");
+    debugPrint("httpGet error: $e");
     return {'statusCode': 503};
+  }
+}
+
+Future<int> httpPost({required String path, Map? data}) async {
+  String baseUrl = 'https://Url 입력하세요$path';
+  var body = jsonEncode(data);
+  try {
+    http.Response response =
+        await http.post(Uri.parse(baseUrl), body: body, headers: {
+      "accept": "application/json",
+      "Content-Type": "application/json",
+    });
+    return response.statusCode;
+  } catch (e) {
+    debugPrint("httpPost error: $e");
+    return 503;
+  }
+}
+
+Future<int> httpPut({required String path, Map? data}) async {
+  String baseUrl = 'https://Url 입력하세요$path';
+  var body = jsonEncode(data);
+  try {
+    http.Response response =
+        await http.post(Uri.parse(baseUrl), body: body, headers: {
+      "accept": "application/json",
+      "Content-Type": "application/json",
+    });
+    return response.statusCode;
+  } catch (e) {
+    debugPrint("httpPut error: $e");
+    return 503;
+  }
+}
+
+Future<int> httpDelete({required String path, Map? data}) async {
+  String baseUrl = 'https://Url 입력하세요$path';
+  var body = jsonEncode(data);
+  try {
+    http.Response response =
+        await http.delete(Uri.parse(baseUrl), body: body, headers: {
+      "accept": "application/json",
+      "Content-Type": "application/json",
+    });
+    return response.statusCode;
+  } catch (e) {
+    debugPrint("httpDelete error: $e");
+
+    return 503;
   }
 }
