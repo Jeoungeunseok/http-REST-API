7afa0ecf486c57bcb3fb876f94e8f985d560968c
Jeoungeunseok <wjddmstjr65@gmail.com>
2023-08-29 09:45:21 +0900

    api data handling

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,45 @@ class _HttpPackagePageState extends State<HttpPackagePage> {
           children: <Widget>[
             ElevatedButton(
                 onPressed: () async {
-                  apiResult = await httpGet(path: '/api/users?page=2');
-                  print(apiResult);
+                  apiResult =
+                      // ignore: body_might_complete_normally_nullable
+                      await httpGet(path: '/api/users?page=2').then((value) {
+                    if (value["statusCode"] == 200) {
+                      debugPrint(value["data"][0]);
+                      return showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('API Result'),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Center(
+                                    child: CircleAvatar(
+                                        radius: 30,
+                                        backgroundImage: NetworkImage(
+                                            value["data"][0]['avatar'])),
+                                  ),
+                                  Text('아이디: ${value["data"][0]['id']}'),
+                                  Text('이메일: ${value["data"][0]['email']}'),
+                                  Text('성: ${value["data"][0]['first_name']}'),
+                                  Text('이름: ${value["data"][0]['last_name']}'),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'))
+                              ],
+                            );
+                          });
+                    } else {
+                      debugPrint('서버 에러입니다. 다시 시도해주세요');
+                    }
+                  });
                 },
                 child: const Text('API CALL'))
           ],
